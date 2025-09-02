ef2d16c1b6bcdd11799c384d5b4f64426caf251d
Felipe Machado <felpsmachadoo@gmail.com>
2025-09-02 18:45:56 -0300

    ✨feat: Adicionando mais informações dos filmes

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,34 @@
 import React, { useEffect, useState } from "react";
 import Tmdb from "./Tmdb";
+import "./App.css";
 import MovieRow from "./components/MovieRow";
+import FeatureMovie from "./components/FeatureMovie";
 
 export default () => {
   const [movieList, setMovieList] = useState([]);
+  const [featureData, setFeatureData] = useState(null);
 
   useEffect(() => {
     const LoadAll = async () => {
       // Pegando a lista TOTAL
       let list = await Tmdb.getHomeList();
       setMovieList(list);
+
+      // Pegando o Featured
+      let originals = list.filter((i) => i.slug === "originals");
+      let randomChosen = Math.floor(
+        Math.random() * (originals[0].items.results.length - 1)
+      );
+      let chosen = originals[0].items.results[randomChosen];
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, "tv");
     };
     LoadAll();
   }, []);
 
   return (
     <div className="page">
+      {featureData && <FeatureMovie item={featureData} />}
+
       <section className="list">
         {movieList.map((item, key) => (
           <MovieRow key={key} title={item.title} items={item.items} />
---
+++ src/components/FeatureMovie.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./FeatureMovie.css";
+
+export default () => {
+  return (
+    <div>
+      <h1>oi</h1>
+    </div>
+  );
+};
