cdbc68e44abea53be570da301aeb32a5f3d807eb
Felipe Machado <felpsmachadoo@gmail.com>
2025-09-02 17:35:03 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from "react";
+import Tmdb from "./Tmdb";
+import MovieRow from "./components/MovieRow";
+
+export default () => {
+  const [movieList, setMovieList] = useState([]);
+
+  useEffect(() => {
+    const LoadAll = async () => {
+      // Pegando a lista TOTAL
+      let list = await Tmdb.getHomeList();
+      setMovieList(list);
+    };
+    LoadAll();
+  }, []);
+
+  return (
+    <div className="page">
+      <section className="list">
+        {movieList.map((item, key) => (
+          <MovieRow key={key} title={item.title} items={item.items} />
+        ))}
+      </section>
+    </div>
+  );
+};
